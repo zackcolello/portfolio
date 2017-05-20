cba54f998435329a98e997b94a50b487dfc94f13
zackcolello <zackcolello@gmail.com>
2017-05-20 15:40:20 -0400

    Update html templates and .gitignore

--- portfolioApp/views.py
+++ portfolioApp/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('../templates/index.html')
     return HttpResponse(template.render(request))
 
